29ec8504a95aa636785dec2c418f39ea26401e3e
josé rodriguez <rodriguezjosetk@hotmail.com>
2018-08-28 23:58:59 -0300

    Comportamiento del SeekBar agregado

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/MainActivity.java
@@ -2,12 +2,46 @@ package ar.edu.utn.frsf.isi.dam.bancolab01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView lblDiasDePlazo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBarDias);
+        lblDiasDePlazo = (TextView) findViewById((R.id.lblDiasDePlazo));
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                lblDiasDePlazo.setText(progress + " dias de plazo");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+
+
+
+
+
 }
